9c9141ccb279ebe581ee19063eb007ea4a257c7f
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-01-25 21:14:46 +0400

    Operation only methods without implementation

---
+++ SE-3114-8/MiniBank.Repository/SqlClientOperationRepository.cs
@@ -0,0 +1,33 @@
+﻿using MiniBank.Models;
+
+namespace MiniBank.Repository
+{
+    public class SqlClientOperationRepository
+    {
+        private const string _connectionString = "Server=DESKTOP-SCSHELD\\SQLEXPRESS;Database=MiniBankSE31148;Trusted_Connection=true;TrustServerCertificate=true";
+        SqlClientAccountRepository _sqlClietnAccountRepository = new();
+
+
+        public async Task Withdraw(int accountId, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Insert(int accountId, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Transfer(int sourceAccountId, int destinationAccountId, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //ახალი ჩანაწერი Operation ცხრილში.
+        public async Task Create(Operation operation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
