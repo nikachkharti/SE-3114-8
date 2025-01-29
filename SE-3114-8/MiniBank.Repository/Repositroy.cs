61e741e2648903db32737b2ef4b64c12299b6ce8
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-01-29 19:58:28 +0400

    Generic repository

--- SE-3114-8/MiniBank.Repository/Repositroy.cs
+++ SE-3114-8/MiniBank.Repository/Repositroy.cs
@@ -14,20 +14,31 @@ namespace MiniBank.Repository
             _connectionString = connectionString;
         }
 
-        public Task<int> Execute(string query, object parameters)
+        public async Task<int> Execute(string query, Dictionary<string, object> parameters, CommandType commandType = CommandType.StoredProcedure)
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.CommandType = commandType;
+
+                    // Add parameters if provided
+                    if (parameters != null)
+                    {
+                        foreach (var param in parameters)
+                        {
+                            command.Parameters.AddWithValue(param.Key, param.Value ?? DBNull.Value);
+                        }
+                    }
+
+                    return await command.ExecuteNonQueryAsync();
+                }
+            }
         }
-
-        public async Task<T> Get(string query, Dictionary<string, object> parameters, CommandType commandType = CommandType.Text)
+        public async Task<T> Get(string query, Dictionary<string, object> parameters, CommandType commandType = CommandType.StoredProcedure)
         {
-            throw new NotImplementedException();
-        }
-
-        public async Task<IEnumerable<T>> GetAll(string query, Dictionary<string, object> parameters = null, CommandType commandType = CommandType.Text)
-        {
-            var results = new List<T>();
-
             using (var connection = new SqlConnection(_connectionString))
             {
                 await connection.OpenAsync();
@@ -47,23 +58,76 @@ namespace MiniBank.Repository
 
                     using (SqlDataReader reader = await command.ExecuteReaderAsync())
                     {
-                        while (await reader.ReadAsync())
+                        if (reader.HasRows)
                         {
-                            T result = new T();
-                            for (int i = 0; i < reader.FieldCount; i++)
+                            if (await reader.ReadAsync()) // Read only one row
                             {
-                                string columnName = reader.GetName(i);
-                                object columnValue = reader.GetValue(i);
+                                T result = new T();
 
-                                // Get the property with the same name as the column name
-                                PropertyInfo prop = typeof(T).GetProperty(columnName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
-
-                                if (prop != null && columnValue != DBNull.Value)
+                                for (int i = 0; i < reader.FieldCount; i++)
                                 {
-                                    prop.SetValue(result, Convert.ChangeType(columnValue, prop.PropertyType));
+                                    string columnName = reader.GetName(i);
+                                    object columnValue = reader.GetValue(i);
+
+                                    // Get the property with the same name as the column name
+                                    PropertyInfo prop = typeof(T).GetProperty(columnName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+
+                                    if (prop != null && columnValue != DBNull.Value)
+                                    {
+                                        prop.SetValue(result, Convert.ChangeType(columnValue, prop.PropertyType));
+                                    }
                                 }
+                                return result;
                             }
-                            results.Add(result);
+                        }
+                    }
+                }
+            }
+            return default; // Return default value if no data is found
+        }
+        public async Task<IEnumerable<T>> GetAll(string query, Dictionary<string, object> parameters = null, CommandType commandType = CommandType.StoredProcedure)
+        {
+            var results = new List<T>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.CommandType = commandType;
+
+                    // Add parameters if provided
+                    if (parameters is not null)
+                    {
+                        foreach (var param in parameters)
+                        {
+                            command.Parameters.AddWithValue(param.Key, param.Value ?? DBNull.Value);
+                        }
+                    }
+
+                    using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                T result = new T();
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    string columnName = reader.GetName(i);
+                                    object columnValue = reader.GetValue(i);
+
+                                    // Get the property with the same name as the column name
+                                    PropertyInfo prop = typeof(T).GetProperty(columnName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+
+                                    if (prop != null && columnValue != DBNull.Value)
+                                    {
+                                        prop.SetValue(result, Convert.ChangeType(columnValue, prop.PropertyType));
+                                    }
+                                }
+                                results.Add(result);
+                            }
                         }
                     }
                 }
